e687751bf03635f7c9fbdb557326f33e86648483
marq wong <marq.wwc@gmail.com>
2024-12-25 21:59:16 +0800

    Step8 Pages & Choice

--- listings/views.py
+++ listings/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Listing
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import price_choices,bedroom_choices,district_choices
 # Create your views here.
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
-    paginator = Paginator(listings,3)
-    page = request.GET.get('page')
+    paginator = Paginator(listings,2)
+    page = request.GET.get("page")
     paged_listings = paginator.get_page(page)
     context = {
-        'listings' : paged_listings
+        'listings':paged_listings
+        }
+    return render(request,'listings/listings.html',context)
+
+def listing(request,listing_id):
+    listing = get_object_or_404(Listing,pk=listing_id)
+    context = {
+        'listing':listing,
     }
-    return render(request,'listings/listings.html', context)
+    return render(request,'listings/listing.html',context)
 
-def listing(request, listing_id):
-    return render(request,'listings/listing.html')
-
-def search(request):
-    return render(request,'listings/search.html')
+def search(request,**kwargs):
+    listings = Listing.objects.order_by('-list_date').filter()
+    context = {
+        'price_choices':price_choices,
+        'bedroom_choices':bedroom_choices,
+        'district_choices':district_choices,
+    }
+    return render(request,'listings/search.html',context)
